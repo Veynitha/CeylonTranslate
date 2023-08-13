59fdb0c672b1d928c5ed284cd1595e8a719fdd79
Veynitha Bandara <veynithabandara@gmail.com>
2023-08-13 20:56:29 +0530

    feature/translate

---
+++ backend/services/translate-api.js
@@ -0,0 +1,47 @@
+const {Translate} = require('@google-cloud/translate').v2;
+require('dotenv').config();
+
+//google api credentials
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+
+//client config
+const translate = new Translate({
+    credentials: CREDENTIALS,
+    projectId: CREDENTIALS.project_id
+});
+
+//detect language method
+const detectLanguage = async (text) => {
+    try {
+        let response = await translate.detect(text);
+        return response[0].language;
+    } catch (error) {
+        console.log(`Error at detect Language --> ${error}`);
+    }
+}
+
+//translate text method
+const translateText = async(text, targetLanguage) => {
+    try {
+        let [response] = await translate.translate(text, targetLanguage);
+        return response;
+    } catch (error) {
+        console.log(`Error at translateText --> ${error}`);
+        return 0;
+    }
+}
+
+//method calling
+detectLanguage('ආයුබෝවන්').then((res)=> {
+    console.log(res)
+}).catch((err) => {
+    console.log(err)
+})
+
+const text = "කොළඹට සාදරයෙන් පිළිගනිමු මිත්‍රයා."
+
+translateText(text, 'en').then((res) => {
+    console.log(res)
+}).catch((err) => {
+    console.log(err)
+})
